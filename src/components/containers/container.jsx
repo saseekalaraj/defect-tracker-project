2259083ea7026cae7448130e6efc55c707f081b7
voranzovv <voranzov@gmail.com>
2019-10-22 04:33:36 +1100

    first commit

--- src/components/containers/container.jsx
+++ src/components/containers/container.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {Fragment} from 'react';
+import 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+// container using material Ui
+// container has property 
+//'textAlight' - left, right, center, justified
 
-const container = () => {
+const container = ({textAlign}) => {
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+             <Container textAlign={textAlign}> </Container>         
+        </Fragment>
     )
 }
 
