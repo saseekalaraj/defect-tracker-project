5ac48b4ae1f96e2036676e41133512810e79e27a
voranzovv <voranzov@gmail.com>
2019-10-22 10:37:28 +1100

    'update'

--- src/App.js
+++ src/App.js
@@ -3,12 +3,90 @@ import logo from './logo.svg';
 import './App.css';
 import Container from './components/containers/container';
 import AppBar from './components/topBar/appBar';
+import Tablee from './components/tables/table';
+import { Divider, Tag } from 'antd';
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <span>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a>Invite {record.name}</a>
+        <Divider type="vertical" />
+        <a>Delete</a>
+      </span>
+    ),
+  },
+];
 
 function App() {
   return (
     <Fragment>
       <Container textAlign="right"/>
       <AppBar position='fixed' backgroundColor='32a852' />
+      <Tablee columns={columns} data={data}/>
     </Fragment>
   );
 }
--- src/components/tables/table.jsx
+++ src/components/tables/table.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
-const table = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from 'react';
+import { Table} from 'antd';
+/* App bar using Ant ui
+* app bar have 2 properties
+   ** columns - headings for table
+   ** data- table data
+*/
+  
+const table = ({data, columns}) => {
+    return ( 
+        <Table columns={columns} dataSource={data} />
+     );
 }
+ 
+export default table;
 
-export default table
+  
+
+
