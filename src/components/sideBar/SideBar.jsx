06ec11da2030d9b187a8c96cc98969c3017b77bc
unknown <sasee9488@gmail.com>
2019-10-24 17:36:46 +0530

    update-site-bar

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -2,87 +2,273 @@ import React, { Component } from 'react'
 import { Menu, Icon, Avatar } from 'antd';
 import { Typography } from '@material-ui/core';
 
-const sideBarContent=[{name:'Dashborad',subMenu:[{name:'Company',url:'/company'}],icon:'user',status:1}]
+// sideBar Menu Object 
+const sideBarContent = [{
+    name: 'Dashborad',
+    subMenu: [
+        {
+            name: 'Company',
+            url: '/company',
+            status: 0,
+        },
+        {
+            name: 'Defect',
+            url: '/defect',
+            status: 0,
+        },
+        {
+            name: 'Developer',
+            url: '/developer',
+            status: 0,
+        },
+        {
+            name: 'Project Manager',
+            url: '/projectManager',
+            status: 0,
+        },
+        {
+            name: 'QA',
+            url: '/qa',
+            status: 0,
+        },
+    ],
+    icon: 'mail',
+    status: 1
+},
+{
+    name: 'Project Administration',
+    icon: 'appstore',
+    status: 0,
+    url: '/projectAdministration'
+},
+{
+    name: 'Company Administration',
+    icon: 'appstore',
+    status: 0,
+    url: '/companyAdministration'
+},
+{
+    name: 'Company',
+    subMenu: [
+        {
+            name: 'Company',
+            url: '/companyCompany',
+            status: 0
+        },
+        {
+            name: 'HR Allocation',
+            url: '/hrAllocation',
+            status: 0
+        },
+        {
+            name: 'Employee',
+            url: '/companyEmployee',
+            status: 0
+        }
+    ],
+    status: 1,
+    icon: 'mail'
+},
+{
+    name: 'Module',
+    icon: 'appstore',
+    status: 0,
+    url: '/module'
+},
+{
+    name: 'Project Allocation',
+    icon: 'appstore',
+    status: 0,
+    url: '/projectAllocation'
+},
+{
+    name: 'Manage Project',
+    icon: 'appstore',
+    status: 0,
+    url: '/manageProject',
+},
+{
+    name: 'Defect',
+    icon: 'mail',
+    status: 1,
+    subMenu: [
+        {
+            name: 'Defect',
+            url: '/defect',
+            status: 0
+        }
+    ]
+},
+{
+    name: 'Setting',
+    icon: 'mail',
+    status: 1,
+    subMenu: [
+        {
+            name: 'General Configuration',
+            url: '/generalConfiguration',
+            status: 0
+        },
+        {
+            name: 'Look and Feel',
+            url: '/lookandfeel',
+            status: 0
+        },
+        {
+            name: 'Profile Setting',
+            url: '/profileSetting',
+            status: 0
+        },
+        {
+            name: 'Common Configuration',
+            subcontent: [
+                {
+                    name: 'Priority',
+                    url: '/priority'
+                },
+                {
+                    name: 'Severity',
+                    url: '/severity'
+                },
+                {
+                    name: 'Defect Type',
+                    url: '/defectType'
+                },
+                {
+                    name: 'Defect Status',
+                    url: '/defectStatus'
+                },
+
+            ],
+            status: 1,
+            icon: 'appstore'
+        },
+        {
+            name: 'Troubleshoot and Support',
+            status: 1,
+            icon: 'appstore',
+            subcontent: [
+                {
+                    name: 'Audit Log',
+                    url: '/auditLog'
+                }
+            ]
+        },
+        {
+            name: 'Privilege',
+            status: 1,
+            icon: 'appstore',
+            subcontent: [
+                {
+                    name: 'Company Privilege',
+                    url: '/companyPrivilege'
+                },
+                {
+                    name: 'Project Privilege',
+                    url: '/projectPrivilege'
+                },
+                {
+                    name: 'QA Lead Privilege',
+                    url: '/qaLeadPrivilege'
+                },
+                {
+                    name: 'Tech Lead Privilege',
+                    url: '/techLeadPrivilege'
+                }
+            ]
+        },
+        {
+            name: 'Work Flow',
+            icon: 'appstore',
+            status: 1,
+            subcontent: [
+                {
+                    name: 'Defect Roles Flow',
+                    url: '/defctRolesFlow'
+                },
+                {
+                    name: 'Defect Status Flow',
+                    url: '/defectStatusFlow'
+                }
+            ]
+        }
+    ]
+}
+]
 const { SubMenu } = Menu;
 class SideBar extends Component {
     render() {
         const { collapsed, onMouseEnterSideBar, sideBarColor, display } = this.props
         return (
-            <div style={{ width: 'auto', marginTop: -64 }} onMouseEnter={() => onMouseEnterSideBar()} onMouseLeave={() => onMouseEnterSideBar()}>
+            <div style={{ width: 'auto', marginTop: -64, overflow: 'auto' }} onMouseEnter={() => onMouseEnterSideBar()} onMouseLeave={() => onMouseEnterSideBar()}>
                 <Menu
-                    style={{ position: 'absolute', height: '100%', width: `${!collapsed ? '250px' : '100px'}`, background: `#${sideBarColor ? sideBarColor : 'fafafa'}`, transition: 'width,0.5s' }}
+                    style={{ position: 'absolute', height: '100%', width: `${!collapsed ? '250px' : '100px'}`, background: `#${sideBarColor ? sideBarColor : 'fafafa'}`, transition: 'width,0.5s', overflow: 'auto' }}
                     defaultSelectedKeys={['1']}
                     mode="inline"
                     inlineCollapsed={collapsed}
                 >
                     <div style={{ height: '20vh', background: '#353c48', textAlign: 'center', justifyContent: 'center', marginBottom: '25%' }}>
                         <div style={{ textAlign: 'center' }}>
-                                <Avatar style={{ marginTop: '25%', transition: 'width,0.5s', border: `${!collapsed ? '5px solid #2d323e' : 'none'}` }} size={!collapsed ? 90 : 70} src="http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg" >
-                                </Avatar>
+                            <Avatar style={{ marginTop: '25%', transition: 'width,0.5s', border: `${!collapsed ? '5px solid #2d323e' : 'none'}` }} size={!collapsed ? 90 : 70} src="http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg" >
+                            </Avatar>
                             <Typography variant="h6" style={{ color: '#048063', display: display }}>
                                 User Name
                             </Typography>
                         </div>
                     </div>
                     {
-                        sideBarContent.map((centent,index)=>
-                            (
-                                <subMenu>
-                                    
-                                </subMenu>
-                            )
+                        sideBarContent.map((content, index) => {
+                            if (content.status === 1) {
+                                return (
+                                    <SubMenu
+                                        key={`sub${index}`}
+                                        title={
+                                            <span>
+                                                <Icon type={content.icon} />
+                                                <span>{content.name}</span>
+                                            </span>
+                                        }
+                                    >
+                                        {content.subMenu.map((submenuContent, subIndex) => {
+                                            if (submenuContent.status === 1) {
+                                                return (
+                                                    <SubMenu key={subIndex} title={
+                                                        <span>
+                                                            <Icon type={submenuContent.icon} />
+                                                            <span>{submenuContent.name}</span>
+                                                        </span>
+                                                    }
+                                                    >
+                                                        {
+                                                            submenuContent.subcontent.map((subContents, indexSub) => {
+                                                                return (
+                                                                    <Menu.Item key={indexSub}>{subContents.name}</Menu.Item>
+                                                                )
+                                                            })
+                                                        }
+                                                    </SubMenu>
+                                                )
+                                            } else {
+                                                return (
+                                                    <Menu.Item key={subIndex}>{submenuContent.name}</Menu.Item>
+                                                )
+                                            }
+                                        })}
+                                    </SubMenu>
+                                )
+                            } else {
+                                return (
+                                    <Menu.Item key={index}>
+                                        <Icon type={content.icon} />
+                                        <span>{content.name}</span>
+                                    </Menu.Item>
+                                )
+                            }
+                        }
                         )
                     }
-                    <SubMenu
-                        key="sub1"
-                        title={
-                            <span>
-                                <Icon type="mail" />
-                                <span>Navigation One</span>
-                            </span>
-                        }
-                    >
-                        <Menu.ItemGroup key="g1" title="Item 1" >
-                            <Menu.Item key="1">Option 1</Menu.Item>
-                            <Menu.Item key="2">Option 2</Menu.Item>
-                        </Menu.ItemGroup>
-                        <Menu.ItemGroup key="g2" title="Item 2">
-                            <Menu.Item key="3">Option 3</Menu.Item>
-                            <Menu.Item key="4">Option 4</Menu.Item>
-                        </Menu.ItemGroup>
-                    </SubMenu>
-                    <SubMenu
-                        key="sub2"
-                        title={
-                            <span>
-                                <Icon type="appstore" />
-                                <span>Navigation Two</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="5">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                        <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="7">Option 7</Menu.Item>
-                            <Menu.Item key="8">Option 8</Menu.Item>
-                        </SubMenu>
-                    </SubMenu>
-                    <SubMenu
-                        key="sub4"
-                        title={
-                            <span>
-                                <Icon type="setting" />
-                                <span>Navigation Three</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
-                        <Menu.Item key="11">Option 11</Menu.Item>
-                        <Menu.Item key="12">Option 12</Menu.Item>
-                    </SubMenu>
                 </Menu>
-            </div>
+            </div >
         )
     }
 }
